5bf286a23b828c8f858899550885ee64888066df
jules20022000 <jules.maglione@epfl.ch>
2022-03-04 23:26:28 +0100

    LogIn SignUp inputs user and new tests

--- app/src/androidTest/java/ch/epfl/sweng/hostme/CreateUserTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/hostme/CreateUserTest.java
@@ -31,10 +31,8 @@ public class CreateUserTest {
     public void checkGenderPage() {
         Intent intent = new Intent(getApplicationContext(), UserCreationPage1.class);
         try (ActivityScenario<UserCreationPage1> scenario = launch(intent)) {
-            onView(withId(R.id.radioGrp)).check(matches(isDisplayed()));
-            onView(withId(R.id.radioM)).check(matches(isDisplayed()));
-            onView(withId(R.id.radioF)).check(matches(isDisplayed()));
             onView(withId(R.id.nextButtonGender)).perform(click());
+            onView(withId(R.id.nextButtonFirstName)).check(matches(isDisplayed()));
         }
     }
 
@@ -43,33 +41,28 @@ public class CreateUserTest {
         Intent intent = new Intent(getApplicationContext(), UserCreationPage2.class);
         try (ActivityScenario<UserCreationPage2> scenario = launch(intent)){
             int nextButtonFirstName = R.id.nextButtonFirstName;
-            onView(withId(nextButtonFirstName)).check(matches(isDisplayed()));
-            onView(withId(R.id.firstName)).check(matches(isDisplayed()));
             onView(withId(nextButtonFirstName)).perform(click());
-        } catch (Exception ignored) {
-
+            onView(withId(R.id.nextButtonLastName)).check(matches(isDisplayed()));
         }
     }
 
-    /*@Test
+    @Test
     public void checkLastNamePage() {
         Intent intent = new Intent(getApplicationContext(), UserCreationPage3.class);
         try (ActivityScenario<UserCreationPage3> scenario = launch(intent)) {
             int nextButtonLastName = R.id.nextButtonLastName;
-            onView(withId(nextButtonLastName)).check(matches(isDisplayed()));
-            onView(withId(R.id.lastName)).check(matches(isDisplayed()));
             onView(withId(nextButtonLastName)).perform(click());
+            onView(withId(R.id.nextButtonMail)).check(matches(isDisplayed()));
         }
-    }*/
+    }
 
     @Test
     public void checkMailPage() {
         Intent intent = new Intent(getApplicationContext(), UserCreationPage4.class);
         try(ActivityScenario<UserCreationPage4> scenario = launch(intent)) {
             int nextButtonMail = R.id.nextButtonMail;
-            onView(withId(nextButtonMail)).check(matches(isDisplayed()));
-            onView(withId(R.id.mail)).check(matches(isDisplayed()));
             onView(withId(nextButtonMail)).perform(click());
+            onView(withId(R.id.terminateButton)).check(matches(isDisplayed()));
         }
     }
 
@@ -79,9 +72,8 @@ public class CreateUserTest {
         try (ActivityScenario<UserCreationPage5> scenario = launch(intent)){
             int terminateButton = R.id.terminateButton;
             onView(withId(terminateButton)).check(matches(isDisplayed()));
-            onView(withId(R.id.password)).check(matches(isDisplayed()));
-            onView(withId(R.id.confirm_pwd)).check(matches(isDisplayed()));
             onView(withId(terminateButton)).perform(click());
+            onView(withId(R.id.welcomeMessage)).check(matches(isDisplayed()));
         }
     }
 

b693696c23c0da43a886f5a9b60f2376cb378463
jules20022000 <jules.maglione@epfl.ch>
2022-03-06 00:21:55 +0100

    LogIn and SignUp Display user inputs but missing some tests

--- app/src/androidTest/java/ch/epfl/sweng/hostme/UserCreation1Test.java
+++ app/src/androidTest/java/ch/epfl/sweng/hostme/UserCreation1Test.java
@@ -3,19 +3,21 @@ package ch.epfl.sweng.hostme;
 import static androidx.test.core.app.ActivityScenario.launch;
 import static androidx.test.core.app.ApplicationProvider.getApplicationContext;
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.clearText;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withHint;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import static org.junit.Assert.assertTrue;
 
 import android.content.Intent;
-import android.os.SystemClock;
 
 import androidx.test.core.app.ActivityScenario;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 
@@ -23,14 +25,20 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-/*
+
 @RunWith(AndroidJUnit4.class)
-public class CreateUserTest {
+public class UserCreation1Test {
 
     @Rule
-    public ActivityScenarioRule<UserCreationPage1> rule =
+    public ActivityScenarioRule<UserCreationPage1> testRule =
             new ActivityScenarioRule<>(UserCreationPage1.class);
 
 
+    @Test
+    public void checkGenderPage() {
+        onView(withId(R.id.nextButtonGender)).perform(click());
+    }
 
-}*/
+
+
+}
--- app/src/androidTest/java/ch/epfl/sweng/hostme/UserCreation5Test.java
+++ app/src/androidTest/java/ch/epfl/sweng/hostme/UserCreation5Test.java
@@ -3,19 +3,21 @@ package ch.epfl.sweng.hostme;
 import static androidx.test.core.app.ActivityScenario.launch;
 import static androidx.test.core.app.ApplicationProvider.getApplicationContext;
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.clearText;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withHint;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import static org.junit.Assert.assertTrue;
 
 import android.content.Intent;
-import android.os.SystemClock;
 
 import androidx.test.core.app.ActivityScenario;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 
@@ -23,14 +25,22 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-/*
+
 @RunWith(AndroidJUnit4.class)
-public class CreateUserTest {
+public class UserCreation5Test {
 
     @Rule
-    public ActivityScenarioRule<UserCreationPage1> rule =
-            new ActivityScenarioRule<>(UserCreationPage1.class);
+    public ActivityScenarioRule<UserCreationPage5> testRule =
+            new ActivityScenarioRule<>(UserCreationPage5.class);
+
+
+    @Test
+    public void checkPwdPage() {
+        int terminateButton = R.id.terminateButton;
+        onView(withId(terminateButton)).check(matches(isDisplayed()));
+        onView(withId(terminateButton)).perform(click());
+    }
 
 
 
-}*/
+}

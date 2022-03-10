a092fd2ef1cb6eaf3d2c2be42587f308702ad9ad
jules20022000 <jules.maglione@epfl.ch>
2022-03-10 10:29:30 +0100

    creation of signUp LogIn page and userInputs + tests

--- app/src/androidTest/java/ch/epfl/sweng/hostme/UserCreationPage3Test.java
+++ app/src/androidTest/java/ch/epfl/sweng/hostme/UserCreationPage3Test.java
@@ -13,6 +13,9 @@ import static androidx.test.espresso.matcher.ViewMatchers.withHint;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import android.content.Intent;
+
+import androidx.test.core.app.ActivityScenario;
 import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
@@ -27,20 +30,56 @@ import org.junit.runner.RunWith;
 @RunWith(AndroidJUnit4.class)
 public class UserCreationPage3Test {
 
-    @Rule
-    public ActivityScenarioRule<UserCreationPage3> rule =
-            new ActivityScenarioRule<>(UserCreationPage3.class);
+    @Test
+    public void checkGenderPage() {
+        Intent intent = new Intent(getApplicationContext(), UserCreationPage1.class);
+        try (ActivityScenario<UserCreationPage1> scenario = launch(intent)) {
+            onView(withId(R.id.nextButtonGender)).perform(click());
+            onView(withId(R.id.nextButtonFirstName)).check(matches(isDisplayed()));
+        }
+    }
+
+    @Test
+    public void checkFirstNamePage() {
+        Intent intent = new Intent(getApplicationContext(), UserCreationPage2.class);
+        try (ActivityScenario<UserCreationPage2> scenario = launch(intent)){
+            int nextButtonFirstName = R.id.nextButtonFirstName;
+            onView(withId(nextButtonFirstName)).perform(click());
+            onView(withId(R.id.nextButtonLastName)).check(matches(isDisplayed()));
+        }
+    }
 
     @Test
     public void checkLastNamePage() {
-        Intents.init();
-
-        onView(withId(R.id.lastName)).perform(clearText()).perform(typeText("Maglione"), closeSoftKeyboard());
-        onView(withId(R.id.lastName)).check(matches(isDisplayed()));
-        onView(withId(R.id.nextButtonLastName)).perform(click());
-
-        Intents.release();
+        Intent intent = new Intent(getApplicationContext(), UserCreationPage3.class);
+        try (ActivityScenario<UserCreationPage3> scenario = launch(intent)) {
+            int nextButtonLastName = R.id.nextButtonLastName;
+            onView(withId(nextButtonLastName)).perform(click());
+            onView(withId(R.id.nextButtonMail)).check(matches(isDisplayed()));
+        }
     }
+
+    @Test
+    public void checkMailPage() {
+        Intent intent = new Intent(getApplicationContext(), UserCreationPage4.class);
+        try(ActivityScenario<UserCreationPage4> scenario = launch(intent)) {
+            int nextButtonMail = R.id.nextButtonMail;
+            onView(withId(nextButtonMail)).perform(click());
+            onView(withId(R.id.terminateButton)).check(matches(isDisplayed()));
+        }
+    }
+
+    @Test
+    public void checkPwdPage() {
+        Intent intent = new Intent(getApplicationContext(), UserCreationPage5.class);
+        try (ActivityScenario<UserCreationPage5> scenario = launch(intent)){
+            int terminateButton = R.id.terminateButton;
+            onView(withId(terminateButton)).check(matches(isDisplayed()));
+            onView(withId(terminateButton)).perform(click());
+            onView(withId(R.id.welcomeMessage)).check(matches(isDisplayed()));
+        }
+    }
+
 }
 
 

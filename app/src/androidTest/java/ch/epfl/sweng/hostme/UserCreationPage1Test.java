7c529faf0d57ac0089a2b070c58ce4169382fa63
Paul Nadal <paul.nadal@epfl.ch>
2022-03-10 20:37:08 +0100

    test CI

--- app/src/androidTest/java/ch/epfl/sweng/hostme/UserCreationPage1Test.java
+++ app/src/androidTest/java/ch/epfl/sweng/hostme/UserCreationPage1Test.java
@@ -20,33 +20,6 @@ import org.junit.runner.RunWith;
 @RunWith(AndroidJUnit4.class)
 public class UserCreationPage1Test {
 
-    @Test
-    public void checkGenderPageMale() {
-
-        Intent intent = new Intent(ApplicationProvider.getApplicationContext(), UserCreationPage1.class);
-
-        try (ActivityScenario<UserCreationPage1> scenario = ActivityScenario.launch(intent)) {
-            onView(withId(R.id.radioM)).check(matches(isDisplayed()));
-            onView(withId(R.id.nextButtonGender)).check(matches(isDisplayed()));
-            onView(withId(R.id.nextButtonGender)).perform(click());
-            onView(withId(R.id.nextButtonFirstName)).check(matches(isDisplayed()));
-        }
-    }
-
-    @Test
-    public void checkGenderPageFemale() {
-
-        Intent intent = new Intent(ApplicationProvider.getApplicationContext(), UserCreationPage1.class);
-
-        try (ActivityScenario<UserCreationPage1> scenario = ActivityScenario.launch(intent)) {
-            onView(withId(R.id.radioF)).check(matches(isDisplayed()));
-            onView(withId(R.id.radioF)).perform(click());
-            onView(withId(R.id.nextButtonGender)).check(matches(isDisplayed()));
-            onView(withId(R.id.nextButtonGender)).perform(click());
-            onView(withId(R.id.nextButtonFirstName)).check(matches(isDisplayed()));
-        }
-    }
-
 }
 
 

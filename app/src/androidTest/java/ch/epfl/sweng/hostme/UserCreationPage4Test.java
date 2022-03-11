1df55d4e6512753c7ddbb9d089caa82e4c958209
jules20022000 <jules.maglione@epfl.ch>
2022-03-11 09:35:16 +0100

    creation of signUp LogIn page and userInputs + tests

---
+++ app/src/androidTest/java/ch/epfl/sweng/hostme/UserCreationPage4Test.java
@@ -0,0 +1,35 @@
+package ch.epfl.sweng.hostme;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+import android.content.Intent;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+@RunWith(AndroidJUnit4.class)
+public class UserCreationPage4Test {
+
+    @Test
+    public void checkMailPage() {
+
+        Intent intent = new Intent(ApplicationProvider.getApplicationContext(), UserCreationPage4.class);
+
+        try (ActivityScenario<UserCreationPage4> scenario = ActivityScenario.launch(intent)) {
+            onView(withId(R.id.nextButtonMail)).check(matches(isDisplayed()));
+            onView(withId(R.id.nextButtonMail)).perform(click());
+        }
+    }
+
+}
+
+

c40a47b63fd80641604642e7ca32c4d5c872773b
jules20022000 <jules.maglione@epfl.ch>
2022-03-11 10:21:32 +0100

    creation of signUp LogIn page and userInputs + tests

--- app/src/main/java/ch/epfl/sweng/hostme/MainActivity.java
+++ app/src/main/java/ch/epfl/sweng/hostme/MainActivity.java
@@ -5,19 +5,32 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.Button;
+import android.widget.EditText;
 
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText userName;
+    private EditText pwd;
+    private Button logInButt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_page);
         Objects.requireNonNull(this.getSupportActionBar()).hide();
 
-        Button logInButt = findViewById(R.id.logInButton);
+        userName = findViewById(R.id.userName);
+        pwd = findViewById(R.id.pwd);
+
+        userName.addTextChangedListener(logInTextWatcher);
+        pwd.addTextChangedListener(logInTextWatcher);
+
+        logInButt = findViewById(R.id.logInButton);
+        logInButt.setEnabled(false);
         logInButt.setOnClickListener(view -> welcome());
 
         Button signUp = findViewById(R.id.signUpButton);
@@ -27,6 +40,26 @@ public class MainActivity extends AppCompatActivity {
         forgotPwd.setOnClickListener(view -> enterMailToChangePwd());
 
     }
+
+    private TextWatcher logInTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            String userNameText = userName.getText().toString().trim();
+            String pwdText = pwd.getText().toString().trim();
+
+            logInButt.setEnabled(!userNameText.isEmpty() && ! pwdText.isEmpty());
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+
+        }
+    };
 
     private void enterMailToChangePwd() {
         Intent intent = new Intent(this, EnterMailChangePwd.class);

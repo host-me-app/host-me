be2f607e49a1a7238467db14e0e09b0ee17bf012
Paul Nadal <paul.nadal@epfl.ch>
2022-03-11 10:48:53 +0100

    add create user and authenticate with firebase

--- app/src/main/java/ch/epfl/sweng/hostme/MainActivity.java
+++ app/src/main/java/ch/epfl/sweng/hostme/MainActivity.java
@@ -3,21 +3,42 @@ package ch.epfl.sweng.hostme;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_page);
+
+        mAuth = FirebaseAuth.getInstance();
+
         Objects.requireNonNull(this.getSupportActionBar()).hide();
 
+        EditText mail = findViewById(R.id.userName);
+        EditText pwd = findViewById(R.id.pwd);
+
         Button logInButt = findViewById(R.id.logInButton);
-        logInButt.setOnClickListener(view -> welcome());
+        logInButt.setOnClickListener(view -> {
+            String mailText = mail.getText().toString();
+            String pwdText = pwd.getText().toString();
+            loginUser(mailText, pwdText);
+        });
 
         Button signUp = findViewById(R.id.signUpButton);
         signUp.setOnClickListener(view -> askUserQuestion());
@@ -42,6 +63,21 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(MainActivity.this, UserCreationPage1.class);
         startActivity(intent);
         overridePendingTransition(R.transition.slide_in_right, R.transition.slide_out_left);
+    }
+
+    private void loginUser(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+        .addOnCompleteListener(this, task -> {
+            if (task.isSuccessful()) {
+                FirebaseUser user = mAuth.getCurrentUser();
+                welcome();
+                Toast.makeText(MainActivity.this, "Authentication successed.",
+                        Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 }
--- app/src/main/java/ch/epfl/sweng/hostme/UserCreationPage5.java
+++ app/src/main/java/ch/epfl/sweng/hostme/UserCreationPage5.java
@@ -4,8 +4,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,11 +23,15 @@ import java.util.concurrent.TimeoutException;
 
 public class UserCreationPage5 extends AppCompatActivity {
     public final static Map<String, String> DATA = new HashMap<>();
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ask_pwd);
+
+        mAuth = FirebaseAuth.getInstance();
+
         Objects.requireNonNull(this.getSupportActionBar()).hide();
 
         Button terminateButt = findViewById(R.id.terminateButton);
@@ -30,7 +42,7 @@ public class UserCreationPage5 extends AppCompatActivity {
             String pwdText = pwd.getText().toString();
             String confirm_pwdText = confirm_pwd.getText().toString();
             if (pwdText.equals(confirm_pwdText) && PasswordValidator.isValid(pwdText)) {
-                welcome();
+                createUser(DATA.get(UserCreationPage4.MAIL), pwdText);
             }
         });
     }
@@ -39,5 +51,20 @@ public class UserCreationPage5 extends AppCompatActivity {
         Intent intent = new Intent(UserCreationPage5.this, WelcomePage.class);
         startActivity(intent);
         overridePendingTransition(R.transition.slide_in_right, R.transition.slide_out_left);
+    }
+
+    private void createUser(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+        .addOnCompleteListener(this, task -> {
+            if (task.isSuccessful()) {
+                FirebaseUser user = mAuth.getCurrentUser();
+                Toast.makeText(UserCreationPage5.this, "Authentication successed.",
+                        Toast.LENGTH_SHORT).show();
+                welcome();
+            } else {
+                Toast.makeText(UserCreationPage5.this, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }

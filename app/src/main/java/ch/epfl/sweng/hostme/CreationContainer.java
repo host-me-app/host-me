95419b0ae513b3712ab1c09b229a138e2141a332
Paul Nadal <paul.nadal@epfl.ch>
2022-03-14 11:27:36 +0100

    test CI

--- app/src/main/java/ch/epfl/sweng/hostme/CreationContainer.java
+++ app/src/main/java/ch/epfl/sweng/hostme/CreationContainer.java
@@ -5,12 +5,16 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
+import java.util.Objects;
+
 public class CreationContainer extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.empty);
+
+        Objects.requireNonNull(this.getSupportActionBar()).hide();
 
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
         fragmentTransaction.add(R.id.fragment_container, new FragmentCreationPage1());

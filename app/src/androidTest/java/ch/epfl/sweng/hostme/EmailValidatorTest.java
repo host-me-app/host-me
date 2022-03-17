61e7ecb852d3d2676c0937e9e8a24619233f34cc
Cetinat <nathanchettrit@hotmail.com>
2022-03-17 15:34:16 +0100

    refactor the implementations of unit tests

--- app/src/androidTest/java/ch/epfl/sweng/hostme/EmailValidatorTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/hostme/EmailValidatorTest.java
@@ -16,24 +16,24 @@ public class EmailValidatorTest {
     @Test
     public void checkEmailIsCorrect() throws ExecutionException, InterruptedException, TimeoutException {
         String email = "test@gmail.com";
-        assertEquals(validator.isValid(email), true);
+        assertEquals(true, validator.isValid(email));
     }
 
     @Test
     public void checkPatternIsIncorrect() throws ExecutionException, InterruptedException, TimeoutException {
         String email = "test@gmail.com.";
-        assertEquals(validator.isValid(email), false);
+        assertEquals(false, validator.isValid(email));
         String email2 = ".test@gmail.com";
-        assertEquals(validator.isValid(email2), false);
+        assertEquals(false, validator.isValid(email2));
         String email3 = "testgmail.com";
-        assertEquals(validator.isValid(email3), false);
+        assertEquals(false, validator.isValid(email3));
         String email4 = "çtêst@gmail.com";
-        assertEquals(validator.isValid(email4), false);
+        assertEquals(false, validator.isValid(email4));
     }
 
     @Test
     public void checkUniquenessIsIncorrect() throws ExecutionException, InterruptedException, TimeoutException {
         String email = "host.me.app2022@gmail.com";
-        assertEquals(validator.isValid(email), false);
+        assertEquals(false, validator.isValid(email));
     }
 }

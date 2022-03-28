fb5d0c6f9312d3b7ee6a51f42033fac7d0cbd852
louiserplh <louise.rieupouilh@gmail.com>
2022-03-28 15:28:02 +0200

    click on contact to display chat activity

---
+++ app/src/main/java/ch/epfl/sweng/hostme/users/UserListener.java
@@ -0,0 +1,5 @@
+package ch.epfl.sweng.hostme.users;
+
+public interface UserListener {
+    void onUserClicked(User user);
+}
--- app/src/main/java/ch/epfl/sweng/hostme/utils/Constants.java
+++ app/src/main/java/ch/epfl/sweng/hostme/utils/Constants.java
@@ -9,4 +9,5 @@ public class Constants {
     public static final String KEY_USER_ID = "userID";
     public static final String KEY_IMAGE = "image";
     public static final String KEY_FCM_TOKEN = "fcmToken";
+    public static final String KEY_USER = "user";
 }

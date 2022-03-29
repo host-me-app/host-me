c9cf7213ab7361357d934b05070844671c846e14
Paul Nadal <paul.nadal@epfl.ch>
2022-03-29 13:00:32 +0200

    init firebase

---
+++ app/src/androidTest/java/ch/epfl/sweng/hostme/loginTest.java
@@ -0,0 +1,58 @@
+package ch.epfl.sweng.hostme;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.clearText;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import android.content.Intent;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.espresso.intent.Intents;
+
+import com.google.firebase.FirebaseApp;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import ch.epfl.sweng.hostme.database.Auth;
+
+public class loginTest {
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        Auth.setTest();
+        FirebaseApp.clearInstancesForTest();
+        FirebaseApp.initializeApp(ApplicationProvider.getApplicationContext());
+        Thread.sleep(1000);
+    }
+
+    @Test
+    public void checkLoginWithValues() throws Exception {
+        Intent intent = new Intent(ApplicationProvider.getApplicationContext(), MainActivity.class);
+        Intents.init();
+        try (ActivityScenario<MainActivity> scenario = ActivityScenario.launch(intent)) {
+            String username = "logintest@gmail.com";
+            String pwd = "fakePassword";
+            onView(withId(R.id.userName)).check(matches(isDisplayed()));
+            onView(withId(R.id.pwd)).check(matches(isDisplayed()));
+
+            onView(withId(R.id.userName)).perform(clearText()).perform(typeText(username), closeSoftKeyboard());
+            onView(withId(R.id.pwd)).perform(clearText()).perform(typeText(pwd), closeSoftKeyboard());
+
+            onView(withId(R.id.userName)).check(matches(withText(username)));
+            onView(withId(R.id.pwd)).check(matches(withText(pwd)));
+
+            onView(withId(R.id.logInButton)).check(matches(isDisplayed()));
+            onView(withId(R.id.logInButton)).perform(click());
+            Thread.sleep(5000);
+        }
+        Intents.release();
+    }
+}
--- app/src/main/java/ch/epfl/sweng/hostme/database/Auth.java
+++ app/src/main/java/ch/epfl/sweng/hostme/database/Auth.java
@@ -1,19 +1,41 @@
 package ch.epfl.sweng.hostme.database;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Auth {
 
+    private static boolean test = false;
+
     public static com.google.android.gms.tasks.Task<com.google.firebase.auth.AuthResult> loginUserWithEmail(String email, String password) {
-        return FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password);
+        return getAdaptedInstance().signInWithEmailAndPassword(email, password);
     }
 
     public static com.google.android.gms.tasks.Task<com.google.firebase.auth.AuthResult> createUser(String email, String password) {
-        return FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password);
+        return getAdaptedInstance().createUserWithEmailAndPassword(email, password);
     }
 
     public static FirebaseAuth getAuth() {
-        return FirebaseAuth.getInstance();
+        return getAdaptedInstance();
+    }
+
+    /**
+     * @return a firebase database that may use a local emulator or not,
+     * depending on state.
+     */
+    public static FirebaseAuth getAdaptedInstance() {
+
+        if (test) {
+            FirebaseAuth fb = FirebaseAuth.getInstance();
+            fb.useEmulator("10.0.2.2", 9099);
+            return fb;
+        } else {
+            return FirebaseAuth.getInstance();
+        }
+    }
+
+    public static void setTest() {
+        test = true;
     }
 
 }

2aab2ee9f780986527dc1c84924da8de7bad43c7
natgate <79515602+natgate@users.noreply.github.com>
2022-04-01 01:22:11 +0200

    Select a profile picture in the profile Page (#60)
    - adds a selection from either Gallery or take a pic with Camera
    - asks permission to Use camera
    - For the moment the pic is not saved either in the database nor on the
      phone

--- app/src/main/java/ch/epfl/sweng/hostme/ui/account/AccountFragment.java
+++ app/src/main/java/ch/epfl/sweng/hostme/ui/account/AccountFragment.java
@@ -1,9 +1,20 @@
 package ch.epfl.sweng.hostme.ui.account;
 
+import static android.provider.MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
 import static androidx.test.core.app.ApplicationProvider.getApplicationContext;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
@@ -11,14 +22,22 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FieldValue;
@@ -50,6 +69,14 @@ public class AccountFragment extends Fragment {
     private String dbLastName;
     private String dbEmail;
     private String dbGender;
+
+    private ImageView editProfilePicture;
+    private FloatingActionButton changePictureButton;
+    private ActivityResultLauncher<Intent> activityResultLauncherGallery;
+    private ActivityResultLauncher<Intent> activityResultLauncherCamera;
+    private static final int CAMERA_PERM_CODE = 101 ;
+
+
     /**
      * Watcher for any modifications of the text in the fields of the profile
      */
@@ -140,12 +167,52 @@ public class AccountFragment extends Fragment {
 
         saveButton.setEnabled(false);
 
+        editProfilePicture = view.findViewById(R.id.userProfileImage);
+        changePictureButton = view.findViewById(R.id.userProfileChangePhotoButton);
 
         mAuth = FirebaseAuth.getInstance();
 
         Button wallet_button = view.findViewById(R.id.wallet_button);
         wallet_button.setOnClickListener(v -> {
             goToWalletFragment();
+        });
+
+        //Image From Camera and Gallery
+        ContentResolver cont = this.getActivity().getContentResolver();
+        activityResultLauncherCamera = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+
+                if (result.getResultCode() == Activity.RESULT_OK) {
+                    if (result.getData() != null) {
+                        Bitmap imageBitmap = (Bitmap) result.getData().getExtras().get("data");
+                        editProfilePicture.setImageBitmap(imageBitmap);
+                    }
+                }
+            }
+        });
+
+        activityResultLauncherGallery = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if (result.getResultCode() == Activity.RESULT_OK) {
+                    if (result.getData()!= null){
+
+                        Uri selectedImage = result.getData().getData();
+                        String[] filePath = { MediaStore.Images.Media.DATA };
+                        Cursor c = cont.query(selectedImage,filePath, null, null, null);
+                        c.moveToFirst();
+                        int columnIndex = c.getColumnIndex(filePath[0]);
+                        String picturePath = c.getString(columnIndex);
+                        c.close();
+                        Bitmap thumbnail = (BitmapFactory.decodeFile(picturePath));
+                        editProfilePicture.setImageBitmap(thumbnail);
+
+                    }
+                }
+            }
         });
 
         DocumentReference docRef = database.collection("users")
@@ -164,6 +231,7 @@ public class AccountFragment extends Fragment {
 
                         addListenerToSaveButton();
                         addListenerToChangePasswordButton();
+                        addListenerToChangePicture();
 
                         logOutButton.setOnClickListener(v -> {
                             logUserOut();
@@ -175,6 +243,87 @@ public class AccountFragment extends Fragment {
 
 
         return view;
+    }
+
+
+    /**
+     * Add a listener to the button Add/Change Picture
+     */
+    private void addListenerToChangePicture() {
+
+        changePictureButton.setOnClickListener(v -> {
+            showImagePickDialog();
+        });
+    }
+
+
+    /**
+     * Showing dialog where you can take select to take image from either
+     * the Gallery or the Camera
+     */
+    private void showImagePickDialog() {
+        String options[] = {"Camera", "Gallery"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(this.getActivity());
+        builder.setTitle("Pick Image From");
+        builder.setItems(options, (dialog, which) -> {
+
+                    if (which == 0) {
+                        pickFromCamera();
+                    }
+                    else if (which == 1){
+                        pickFromGallery();
+                    }
+                }
+        );
+        builder.create().show();
+    }
+
+
+    /**
+     * Pick user Profile image from Gallery
+     */
+    private void pickFromGallery() {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK,EXTERNAL_CONTENT_URI);
+        galleryIntent.setType("image/*");
+        activityResultLauncherGallery.launch(galleryIntent);
+    }
+
+    /**
+     * Pick user Profile image from Camera
+     */
+    private void pickFromCamera() {
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA) !=
+                PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CAMERA}, CAMERA_PERM_CODE);
+        } else {
+            openCamera();
+        }
+    }
+
+    /** Only for The camera
+     * @param requestCode The code of the request
+     * @param permissions The permissions that are given
+     * @param grantResults The result of the permission
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == CAMERA_PERM_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openCamera();
+            } else {
+                Toast.makeText(getActivity(), "Camera Permission is Required to Use Camera", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    /**
+     * Open Camera
+     */
+    private void openCamera(){
+        Toast.makeText(getActivity(), "Camera Open Request",Toast.LENGTH_SHORT).show();
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        activityResultLauncherCamera.launch(cameraIntent);
     }
 
     /**

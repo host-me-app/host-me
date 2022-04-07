da02ba16664f66cce2a3b96a6834e1dd7e5ff7a1
Louise RIEUPOUILH <44434215+louiserplh@users.noreply.github.com>
2022-04-07 20:31:41 +0200

    Chat/sending messages (#72)
    * chat preparation, adding adaptater
    
    * minor changes in layout messages and implementation chat
    
    * fix issues in appartment for build and minor mistakes
    
    * fixing errors in chat activity but doesn't work yet
    
    * using Database class and updating rules on firebase
    
    * changing deprecated methods in chatActivity
    
    * changing comparator using level 24 API
    
    * test ui for userActivity and MessageFrag
    
    Co-authored-by: Paul Nadal <58035866+paul-ndl@users.noreply.github.com>

---
+++ app/src/main/java/ch/epfl/sweng/hostme/chat/ChatAdapter.java
@@ -0,0 +1,102 @@
+package ch.epfl.sweng.hostme.chat;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import ch.epfl.sweng.hostme.databinding.ItemContainerReceivedMessageBinding;
+import ch.epfl.sweng.hostme.databinding.ItemContainerSentMessageBinding;
+
+public class ChatAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+
+    private final List<ChatMessage> chatMessages;
+    private final String senderId;
+
+    public static final int VIEW_TYPE_SENT = 1;
+    public static final int VIEW_TYPE_RECEIVED = 2;
+
+    public ChatAdapter(List<ChatMessage> chatMessages, String senderId) {
+        this.chatMessages = chatMessages;
+        this.senderId = senderId;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if(viewType == VIEW_TYPE_SENT){
+            return new SentMessageViewHolder(
+                    ItemContainerSentMessageBinding.inflate(
+                            LayoutInflater.from(parent.getContext()),
+                            parent,
+                            false
+                    )
+            );
+        }else{
+            return new ReceivedMessageViewHolder(
+                    ItemContainerReceivedMessageBinding.inflate(
+                            LayoutInflater.from(parent.getContext()),
+                            parent,
+                            false
+                    )
+            );
+        }
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        if(getItemViewType(position) == VIEW_TYPE_SENT){
+            ((SentMessageViewHolder) holder).setData(chatMessages.get(position));
+        }else{
+            ((ReceivedMessageViewHolder) holder).setData(chatMessages.get(position));
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return chatMessages.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if(chatMessages.get(position).senderId.equals(senderId)){
+            return VIEW_TYPE_SENT;
+        }else {
+            return VIEW_TYPE_RECEIVED;
+        }
+    }
+
+    static class SentMessageViewHolder extends RecyclerView.ViewHolder{
+
+        private final ItemContainerSentMessageBinding binding;
+
+        SentMessageViewHolder(ItemContainerSentMessageBinding itemContainerSentMessageBinding){
+            super(itemContainerSentMessageBinding.getRoot());
+            binding = itemContainerSentMessageBinding;
+        }
+
+        void setData(ChatMessage chatMessage){
+            binding.textMessage.setText(chatMessage.message);
+            binding.messageDate.setText(chatMessage.dateTime);
+        }
+    }
+
+    static class ReceivedMessageViewHolder extends RecyclerView.ViewHolder{
+
+        private final ItemContainerReceivedMessageBinding binding;
+
+        ReceivedMessageViewHolder(ItemContainerReceivedMessageBinding itemContainerReceivedMessageBinding){
+            super(itemContainerReceivedMessageBinding.getRoot());
+            binding = itemContainerReceivedMessageBinding;
+        }
+
+        void setData(ChatMessage chatMessage){
+            binding.textMessage.setText(chatMessage.message);
+            binding.messageDate.setText(chatMessage.dateTime);
+        }
+    }
+}

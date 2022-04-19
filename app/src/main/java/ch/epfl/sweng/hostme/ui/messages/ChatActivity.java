e316814228fae48362f34122e766561f2be295e1
jules20022000 <jules.maglione@epfl.ch>
2022-04-19 11:32:30 +0200

    beginning implementation of the call feature

--- app/src/main/java/ch/epfl/sweng/hostme/ui/messages/ChatActivity.java
+++ app/src/main/java/ch/epfl/sweng/hostme/ui/messages/ChatActivity.java
@@ -1,15 +1,25 @@
 package ch.epfl.sweng.hostme.ui.messages;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.SurfaceView;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.text.SimpleDateFormat;
@@ -19,9 +29,11 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 import java.util.Objects;
 
 import ch.epfl.sweng.hostme.MenuActivity;
+import ch.epfl.sweng.hostme.R;
 import ch.epfl.sweng.hostme.chat.ChatAdapter;
 import ch.epfl.sweng.hostme.chat.ChatMessage;
 import ch.epfl.sweng.hostme.database.Auth;
@@ -29,6 +41,11 @@ import ch.epfl.sweng.hostme.database.Database;
 import ch.epfl.sweng.hostme.databinding.ActivityChatBinding;
 import ch.epfl.sweng.hostme.users.User;
 import ch.epfl.sweng.hostme.utils.Constants;
+import io.agora.rtc.IRtcEngineEventHandler;
+import io.agora.rtc.RtcEngine;
+import io.agora.rtc.video.VideoCanvas;
+import io.agora.rtc.video.VideoEncoderConfiguration;
+
 
 public class ChatActivity extends AppCompatActivity {
 
@@ -38,6 +55,17 @@ public class ChatActivity extends AppCompatActivity {
     private User receiverUser;
     private List<ChatMessage> chatMessages;
     private ChatAdapter chatAdapter;
+
+    private static final int PERMISSION_REQ_ID = 22;
+    private static final String[] REQUESTED_PERMISSIONS = {Manifest.permission.RECORD_AUDIO,
+            Manifest.permission.CAMERA, Manifest.permission.BLUETOOTH_CONNECT};
+    private RtcEngine mRtcEngine;
+    private ImageView joinButt;
+    private ImageView audioButt;
+    private ImageView leaveButt;
+    private ImageView videoButt;
+    private final static String currUserID = Auth.getUid();
+    private final CollectionReference reference = Database.getCollection("users");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +77,22 @@ public class ChatActivity extends AppCompatActivity {
         loadReceiverDetails();
         init();
         listenMessages();
+        if (checkSelfPermission(REQUESTED_PERMISSIONS[0], PERMISSION_REQ_ID) &&
+                checkSelfPermission(REQUESTED_PERMISSIONS[1], PERMISSION_REQ_ID)) {
+            initAgoraEngine();
+        }
+        findViewById(R.id.bg_video_container).setVisibility(View.GONE);
+        audioButt = findViewById(R.id.audioBtn);
+        audioButt.setVisibility(View.GONE); // set the audio button hidden
+        audioButt.setOnClickListener(l -> onAudioMuteClicked(audioButt));
+        leaveButt = findViewById(R.id.leaveBtn);
+        leaveButt.setVisibility(View.GONE); // set the leave button hidden
+        leaveButt.setOnClickListener(l -> onLeaveChannelClicked());
+        videoButt = findViewById(R.id.videoBtn);
+        videoButt.setVisibility(View.GONE); // set the video button hidden
+        videoButt.setOnClickListener(l -> onVideoMuteClicked(videoButt));
+        joinButt = findViewById(R.id.joinBtn);
+        joinButt.setOnClickListener(l -> onJoinChannelClicked());
     }
 
     private void init(){
@@ -132,7 +176,192 @@ public class ChatActivity extends AppCompatActivity {
     }
 
     @NonNull
-    private String getReadableDataTime(Date date){
+    private String getReadableDataTime(Date date) {
         return new SimpleDateFormat("MMMM dd, yyyy - hh:mm a", Locale.getDefault()).format(date);
     }
+
+    // --- beginning of call implementation
+
+    public boolean checkSelfPermission(String permission, int requestCode) {
+        if (ContextCompat.checkSelfPermission(this,
+                permission)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    REQUESTED_PERMISSIONS,
+                    requestCode);
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[], @NonNull int[] grantResults) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_REQ_ID) {
+            if (grantResults[0] != PackageManager.PERMISSION_GRANTED || grantResults[1] != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            // if permission granted, initialize the engine
+            initAgoraEngine();
+        }
+    }
+
+    private void initAgoraEngine() {
+        try {
+            mRtcEngine = RtcEngine.create(getApplicationContext(), getString(R.string.agora_app_id), mRtcEventHandler);
+        } catch (Exception e) {
+            throw new RuntimeException("NEED TO check rtc sdk init fatal error\n" + Log.getStackTraceString(e));
+        }
+        setupSession();
+    }
+
+    private void setupSession() {
+        mRtcEngine.setChannelProfile(io.agora.rtc.Constants.CHANNEL_PROFILE_COMMUNICATION);
+
+        mRtcEngine.enableVideo();
+
+        mRtcEngine.setVideoEncoderConfiguration(new VideoEncoderConfiguration(VideoEncoderConfiguration.VD_640x480, VideoEncoderConfiguration.FRAME_RATE.FRAME_RATE_FPS_30,
+                VideoEncoderConfiguration.STANDARD_BITRATE,
+                VideoEncoderConfiguration.ORIENTATION_MODE.ORIENTATION_MODE_FIXED_PORTRAIT));
+    }
+
+    private void setupLocalVideoFeed() {
+        FrameLayout videoContainer = findViewById(R.id.floating_video_container);
+        videoContainer.setVisibility(View.VISIBLE);
+        SurfaceView videoSurface = RtcEngine.CreateRendererView(getBaseContext());
+        videoSurface.setZOrderMediaOverlay(true);
+        videoContainer.addView(videoSurface);
+        mRtcEngine.setupLocalVideo(new VideoCanvas(videoSurface, VideoCanvas.RENDER_MODE_FIT, 0));
+    }
+
+    private void setupRemoteVideoStream(int uid) {
+        FrameLayout videoContainer = findViewById(R.id.bg_video_container);
+        videoContainer.setVisibility(View.VISIBLE);
+        SurfaceView videoSurface = RtcEngine.CreateRendererView(getBaseContext());
+        videoContainer.addView(videoSurface);
+        mRtcEngine.setupRemoteVideo(new VideoCanvas(videoSurface, VideoCanvas.RENDER_MODE_FIT, uid));
+        mRtcEngine.setRemoteSubscribeFallbackOption(io.agora.rtc.Constants.STREAM_FALLBACK_OPTION_AUDIO_ONLY);
+    }
+
+    // Handle SDK Events
+    private final IRtcEngineEventHandler mRtcEventHandler = new IRtcEngineEventHandler() {
+
+        @Override
+        public void onJoinChannelSuccess(String channel, int uid, int elapsed) {
+            runOnUiThread(() -> Toast.makeText(getApplicationContext(),
+                    "Join channel success, uid: " + (uid & 0xFFFFFFFFL), Toast.LENGTH_SHORT).show());
+        }
+
+        @Override
+        public void onUserJoined(final int uid, int elapsed) {
+            runOnUiThread(() -> setupRemoteVideoStream(uid));
+        }
+
+        // remote user has left channel
+        @Override
+        public void onUserOffline(int uid, int reason) {
+            runOnUiThread(() -> onRemoteUserLeft());
+        }
+
+        // remote user has toggled their video
+        @Override
+        public void onRemoteVideoStateChanged(final int uid, final int state, int reason, int elapsed) {
+            runOnUiThread(() -> onRemoteUserVideoToggle(state));
+        }
+    };
+
+    private void onRemoteUserVideoToggle(int state) {
+        FrameLayout videoContainer = findViewById(R.id.bg_video_container);
+        videoContainer.setVisibility(View.VISIBLE);
+
+        SurfaceView videoSurface = (SurfaceView) videoContainer.getChildAt(0);
+        videoSurface.setVisibility(state == 0 ? View.GONE : View.VISIBLE);
+
+        // add an icon to let the other user know remote video has been disabled
+        if (state == 0) {
+            ImageView noCamera = new ImageView(this);
+            noCamera.setImageResource(R.drawable.video_disabled);
+            videoContainer.addView(noCamera);
+        } else {
+            ImageView noCamera = (ImageView) videoContainer.getChildAt(1);
+            if (noCamera != null) {
+                videoContainer.removeView(noCamera);
+            }
+        }
+    }
+
+    private void onRemoteUserLeft() {
+        removeVideo(R.id.bg_video_container);
+    }
+
+
+    public void onJoinChannelClicked() {
+        String channelName = currUserID;
+        Map<String, Object> docRoom = new HashMap<>();
+        mRtcEngine.joinChannel(null, channelName, null, 0); // if you do not specify the uid, Agora will assign one.
+        setupLocalVideoFeed();
+        docRoom.put("roomName", channelName);
+        reference.document(receiverUser.id).update(docRoom);
+        findViewById(R.id.joinBtn).setVisibility(View.GONE); // set the join button hidden
+        findViewById(R.id.audioBtn).setVisibility(View.VISIBLE); // set the audio button hidden
+        findViewById(R.id.leaveBtn).setVisibility(View.VISIBLE); // set the leave button hidden
+        findViewById(R.id.videoBtn).setVisibility(View.VISIBLE); // set the video button hidden
+    }
+
+    public void onLeaveChannelClicked() {
+        leaveChannel();
+        reference.document(receiverUser.id).update("roomName", FieldValue.arrayRemove(currUserID));
+        removeVideo(R.id.floating_video_container);
+        removeVideo(R.id.bg_video_container);
+        findViewById(R.id.joinBtn).setVisibility(View.VISIBLE); // set the join button visible
+        findViewById(R.id.audioBtn).setVisibility(View.GONE); // set the audio button hidden
+        findViewById(R.id.leaveBtn).setVisibility(View.GONE); // set the leave button hidden
+        findViewById(R.id.videoBtn).setVisibility(View.GONE); // set the video button hidden
+    }
+
+    private void leaveChannel() {
+        mRtcEngine.leaveChannel();
+    }
+
+    private void removeVideo(int containerID) {
+        FrameLayout videoContainer = findViewById(containerID);
+        videoContainer.setVisibility(View.GONE);
+        videoContainer.removeAllViews();
+    }
+
+    public void onAudioMuteClicked(View view) {
+        ImageView btn = (ImageView) view;
+        if (btn.isSelected()) {
+            btn.setSelected(false);
+            btn.setImageResource(R.drawable.audio_toggle_btn);
+        } else {
+            btn.setSelected(true);
+            btn.setImageResource(R.drawable.audio_toggle_active_btn);
+        }
+
+        mRtcEngine.muteLocalAudioStream(btn.isSelected());
+    }
+
+    public void onVideoMuteClicked(View view) {
+        ImageView btn = (ImageView) view;
+        if (btn.isSelected()) {
+            btn.setSelected(false);
+            btn.setImageResource(R.drawable.video_toggle_btn);
+        } else {
+            btn.setSelected(true);
+            btn.setImageResource(R.drawable.video_toggle_active_btn);
+        }
+
+        mRtcEngine.muteLocalVideoStream(btn.isSelected());
+
+        FrameLayout container = findViewById(R.id.floating_video_container);
+        container.setVisibility(btn.isSelected() ? View.GONE : View.VISIBLE);
+        SurfaceView videoSurface = (SurfaceView) container.getChildAt(0);
+        videoSurface.setZOrderMediaOverlay(!btn.isSelected());
+        videoSurface.setVisibility(btn.isSelected() ? View.GONE : View.VISIBLE);
+    }
+
 }

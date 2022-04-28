539aeb0a265e70f9add268e2a101d74e0ee58162
jules20022000 <jules.maglione@epfl.ch>
2022-04-28 10:14:53 +0200

    add button to call user, joinChannel and send notification

---
+++ app/src/main/java/ch/epfl/sweng/hostme/ui/messages/CallService.java
@@ -0,0 +1,8 @@
+package ch.epfl.sweng.hostme.ui.messages;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class CallService extends FirebaseMessagingService {
+
+
+}
--- app/src/main/java/ch/epfl/sweng/hostme/ui/messages/MessageService.java
+++ app/src/main/java/ch/epfl/sweng/hostme/ui/messages/MessageService.java
@@ -1,19 +1,80 @@
 package ch.epfl.sweng.hostme.ui.messages;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
 public class MessageService extends FirebaseMessagingService {
+    NotificationManager mNotificationManager;
 
     @Override
     public void onNewToken(@NonNull String token) {
         super.onNewToken(token);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.S)
     @Override
-    public void onMessageReceived(@NonNull RemoteMessage message) {
-        super.onMessageReceived(message);
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+
+        Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), notification);
+        r.play();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            r.setLooping(false);
+        }
+        int resourceImage = getResources().getIdentifier(remoteMessage.getNotification().getIcon(), "drawable", getPackageName());
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "CHANNEL_ID");
+        builder.setSmallIcon(resourceImage);
+
+
+        Intent resultIntent = new Intent(this, JoinCall.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addNextIntentWithParentStack(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(0,
+                        PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+
+
+        builder.setContentTitle(remoteMessage.getNotification().getTitle());
+        builder.setContentText(remoteMessage.getNotification().getBody());
+        builder.setContentIntent(resultPendingIntent);
+        builder.setStyle(new NotificationCompat.BigTextStyle().bigText(remoteMessage.getNotification().getBody()));
+        builder.setAutoCancel(true);
+        builder.setPriority(Notification.PRIORITY_MAX);
+
+        mNotificationManager =
+                (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String channelId = "Your_channel_id";
+            NotificationChannel channel = new NotificationChannel(
+                    channelId,
+                    "Channel human readable title",
+                    NotificationManager.IMPORTANCE_HIGH);
+            mNotificationManager.createNotificationChannel(channel);
+            builder.setChannelId(channelId);
+        }
+
+        mNotificationManager.notify(100, builder.build());
+
     }
 }

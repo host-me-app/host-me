503d94c000786089e72ec32cafe1622333e5196c
jules20022000 <jules.maglione@epfl.ch>
2022-05-02 10:18:41 +0200

    add a rule in a test class and just log in to see to check Cirrus

--- app/src/androidTest/java/ch/epfl/sweng/hostme/chat/CallTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/hostme/chat/CallTest.java
@@ -12,6 +12,7 @@ import android.content.Intent;
 
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.espresso.intent.Intents;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.GrantPermissionRule;
 
 import com.google.firebase.FirebaseApp;
@@ -19,6 +20,7 @@ import com.google.firebase.FirebaseApp;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import ch.epfl.sweng.hostme.MainActivity;
 import ch.epfl.sweng.hostme.R;
@@ -26,6 +28,7 @@ import ch.epfl.sweng.hostme.database.Auth;
 import ch.epfl.sweng.hostme.database.Database;
 import ch.epfl.sweng.hostme.database.Storage;
 
+@RunWith(AndroidJUnit4.class)
 public class CallTest {
 
     @BeforeClass

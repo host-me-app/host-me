5949f89cb201d31b376b81aaa748214c84a12dab
Paul Nadal <58035866+paul-ndl@users.noreply.github.com>
2022-05-05 20:28:53 +0200

    Filter apartments around current location (GPS sensor) (#136)
    * filter with GPS
    
    * fix clear filters
    
    * test filter with GPS
    
    * test filter with GPS

--- app/src/androidTest/java/ch/epfl/sweng/hostme/DisplayApartmentTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/hostme/DisplayApartmentTest.java
@@ -5,23 +5,29 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.Espresso.pressBack;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.swipeRight;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
+import android.Manifest;
+import android.app.Fragment;
 import android.content.Intent;
+import android.widget.Switch;
 
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.espresso.intent.Intents;
+import androidx.test.rule.GrantPermissionRule;
 
 import com.google.firebase.FirebaseApp;
 
 import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
 
 import ch.epfl.sweng.hostme.database.Auth;
@@ -29,6 +35,11 @@ import ch.epfl.sweng.hostme.database.Database;
 import ch.epfl.sweng.hostme.database.Storage;
 
 public class DisplayApartmentTest {
+
+    @Rule
+    public GrantPermissionRule internetRule =
+            GrantPermissionRule.grant(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION);
+
 
     @BeforeClass
     public static void setUp() {
@@ -107,6 +118,29 @@ public class DisplayApartmentTest {
             onView(withId(R.id.all_filters)).check(matches(isDisplayed()));
             onView(withId(R.id.filters)).perform(click());
             Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Intents.release();
+    }
+
+    @Test
+    public void filterWithGPS() {
+        Intent intent = new Intent(ApplicationProvider.getApplicationContext(), MainActivity.class);
+        Intents.init();
+        try (ActivityScenario<MainActivity> scenario = ActivityScenario.launch(intent)) {
+            String mail = "testlogin@gmail.com";
+            String password = "fakePassword1!";
+
+            onView(withId(R.id.userName)).perform(typeText(mail), closeSoftKeyboard());
+            onView(withId(R.id.pwd)).perform(typeText(password), closeSoftKeyboard());
+            onView(withId(R.id.logInButton)).perform(click());
+            Thread.sleep(1000);
+
+            onView(withId(R.id.filters)).perform(click());
+            onView(withId(R.id.gpsSwitch)).perform(click());
+            onView(withId(R.id.filters)).perform(click());
+            Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }

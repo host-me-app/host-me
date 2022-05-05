5b07a5a485d6da6f491510368b5aa092e7b62f29
Cetinat <nathanchettrit@hotmail.com>
2022-05-05 20:19:24 +0200

    Added tests for expiration date picker

---
+++ app/src/androidTest/java/ch/epfl/sweng/hostme/wallet/DocumentExpirationDateTest.java
@@ -0,0 +1,61 @@
+package ch.epfl.sweng.hostme.wallet;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import ch.epfl.sweng.hostme.R;
+
+public class DocumentExpirationDateTest {
+
+    @Test
+    public void ResidenceDescriptionFieldTextIdIsSame() {
+        assertEquals(R.id.textExpirationDate1, DocumentExpirationDate.RESIDENCE_PERMIT.getDescriptionFieldTextId());
+    }
+
+    @Test
+    public void ResidenceExpirationDateTextIdIsSame() {
+        assertEquals(R.id.textExpirationDate_ResidencePermit, DocumentExpirationDate.RESIDENCE_PERMIT.getExpirationDateTextId());
+    }
+
+    @Test
+    public void ResidencePickDateButtonIdIsSame() {
+        assertEquals(R.id.buttonPickDate_ResidencePermit, DocumentExpirationDate.RESIDENCE_PERMIT.getPickDateButtonId());
+    }
+
+
+    @Test
+    public void SalaryDescriptionFieldTextIdIsSame() {
+        assertEquals(R.id.textExpirationDate2, DocumentExpirationDate.SALARY_SLIPS.getDescriptionFieldTextId());
+    }
+
+    @Test
+    public void SalaryExpirationDateTextIdIsSame() {
+        assertEquals(R.id.textExpirationDate_SalarySlips, DocumentExpirationDate.SALARY_SLIPS.getExpirationDateTextId());
+    }
+
+    @Test
+    public void SalaryPickDateButtonIdIsSame() {
+        assertEquals(R.id.buttonPickDate_SalarySlips, DocumentExpirationDate.SALARY_SLIPS.getPickDateButtonId());
+    }
+
+
+
+    @Test
+    public void ExeOfficeDescriptionFieldTextIdIsSame() {
+        assertEquals(R.id.textExpirationDate3, DocumentExpirationDate.EXECUTION_OFFICE.getDescriptionFieldTextId());
+    }
+
+    @Test
+    public void ExeOfficeExpirationDateTextIdIsSame() {
+        assertEquals(R.id.textExpirationDate_ExecOffice, DocumentExpirationDate.EXECUTION_OFFICE.getExpirationDateTextId());
+    }
+
+    @Test
+    public void ExeOfficePickDateButtonIdIsSame() {
+        assertEquals(R.id.buttonPickDate_ExecOffice, DocumentExpirationDate.EXECUTION_OFFICE.getPickDateButtonId());
+    }
+
+
+}
---
+++ app/src/androidTest/java/ch/epfl/sweng/hostme/wallet/ExpirationDatePickingTest.java
@@ -0,0 +1,121 @@
+package ch.epfl.sweng.hostme.wallet;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+import android.content.Intent;
+import android.widget.DatePicker;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.espresso.contrib.PickerActions;
+import androidx.test.espresso.intent.Intents;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.GrantPermissionRule;
+
+import com.google.firebase.FirebaseApp;
+
+import org.hamcrest.Matchers;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import ch.epfl.sweng.hostme.MainActivity;
+import ch.epfl.sweng.hostme.R;
+import ch.epfl.sweng.hostme.database.Auth;
+import ch.epfl.sweng.hostme.database.Database;
+import ch.epfl.sweng.hostme.database.Storage;
+
+
+@RunWith(AndroidJUnit4.class)
+public class ExpirationDatePickingTest {
+
+    @Rule
+    public GrantPermissionRule internetRule = GrantPermissionRule.grant(android.Manifest.permission.INTERNET);
+
+    @BeforeClass
+    public static void setUp() {
+        Auth.setTest();
+        Database.setTest();
+        Storage.setTest();
+        FirebaseApp.clearInstancesForTest();
+        FirebaseApp.initializeApp(ApplicationProvider.getApplicationContext());
+    }
+
+
+    @Test
+    public void NoExpirationDateOnDocumentTest() {
+
+        Intent intent = new Intent(ApplicationProvider.getApplicationContext(), MainActivity.class);
+        Intents.init();
+        try (ActivityScenario<MainActivity> scenario = ActivityScenario.launch(intent)) {
+            String mail = "testlogin@gmail.com";
+            String password = "fakePassword1!";
+
+            onView(withId(R.id.userName)).perform(typeText(mail), closeSoftKeyboard());
+            onView(withId(R.id.pwd)).perform(typeText(password), closeSoftKeyboard());
+            onView(withId(R.id.logInButton)).perform(click());
+            Thread.sleep(1000);
+
+            onView(withId(R.id.navigation_account)).perform(click());
+            Thread.sleep(1000);
+
+            onView(withId(R.id.wallet_button)).perform(click());
+            Thread.sleep(1000);
+            onView(withId(R.id.textExpirationDate_ResidencePermit)).check(matches(allOf(withText("None"),
+                    isDisplayed())));
+
+            Thread.sleep(5000);
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Intents.release();
+    }
+
+    @Test
+    public void expirationDatePickingTest() {
+        Intent intent = new Intent(ApplicationProvider.getApplicationContext(), MainActivity.class);
+        Intents.init();
+        try (ActivityScenario<MainActivity> scenario = ActivityScenario.launch(intent)) {
+            String mail = "testlogin@gmail.com";
+            String password = "fakePassword1!";
+
+            onView(withId(R.id.userName)).perform(typeText(mail), closeSoftKeyboard());
+            onView(withId(R.id.pwd)).perform(typeText(password), closeSoftKeyboard());
+            onView(withId(R.id.logInButton)).perform(click());
+            Thread.sleep(1000);
+
+            onView(withId(R.id.navigation_account)).perform(click());
+            Thread.sleep(1000);
+
+            onView(withId(R.id.wallet_button)).perform(click());
+            Thread.sleep(1000);
+            onView(withId(R.id.buttonPickDate_ResidencePermit)).perform(click());
+            Thread.sleep(1000);
+            onView(withClassName(Matchers.equalTo(DatePicker.class.getName())))
+                    .perform(PickerActions.setDate(2022, 3, 22));
+            Thread.sleep(1000);
+            onView(withId(android.R.id.button1)).perform(click());
+            Thread.sleep(1000);
+
+            onView(withId(R.id.textExpirationDate_ResidencePermit)).check(matches(allOf(withText("22/3/2022"),
+                    isDisplayed())));
+
+            Thread.sleep(5000);
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Intents.release();
+    }
+}

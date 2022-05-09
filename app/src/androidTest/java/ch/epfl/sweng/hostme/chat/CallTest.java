1460a8023885bc9d706b3467c8e18034d924b0fa
jules20022000 <jules.maglione@epfl.ch>
2022-05-09 22:56:45 +0200

    call test

--- app/src/androidTest/java/ch/epfl/sweng/hostme/chat/CallTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/hostme/chat/CallTest.java
@@ -68,8 +68,9 @@ public class CallTest {
                     RecyclerViewActions.actionOnItemAtPosition(0, click()));
 
             onView(withId(R.id.launchButt)).perform(click());
-            /*Thread.sleep(2000);
-            onView(withId(R.id.audioBtn)).perform(click());
+            Thread.sleep(2000);
+            onView(withId(R.id.leaveBtn)).perform(click());
+            /*onView(withId(R.id.audioBtn)).perform(click());
             onView(withId(R.id.audioBtn)).perform(click());
             onView(withId(R.id.audioBtn)).perform(click());
             onView(withId(R.id.videoBtn)).perform(click());
@@ -77,7 +78,6 @@ public class CallTest {
             onView(withId(R.id.switch_camera)).perform(click());
             onView(withId(R.id.switch_camera)).perform(click());
             Thread.sleep(500);*/
-            //onView(withId(R.id.leaveBtn)).perform(click());
         } catch (InterruptedException e) {
             e.printStackTrace();
         }

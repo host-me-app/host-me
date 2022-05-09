3115cff6b2c0ac2c119c9b9f0e2e345e2890f3b8
jules20022000 <jules.maglione@epfl.ch>
2022-05-10 00:29:55 +0200

    call test

--- app/src/androidTest/java/ch/epfl/sweng/hostme/wallet/ExpirationDatePickingTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/hostme/wallet/ExpirationDatePickingTest.java
@@ -61,7 +61,7 @@ public class ExpirationDatePickingTest {
             onView(withId(R.id.navigation_account)).perform(click());
             Thread.sleep(1000);
 
-            onView(withId(R.id.wallet_button)).perform(click());
+            //onView(withId(R.id.wallet_button)).perform(click());
             /*onView(withId(R.id.buttonPickDate_ResidencePermit)).perform(click());
             Thread.sleep(1000);
             onView(withClassName(Matchers.equalTo(DatePicker.class.getName())))

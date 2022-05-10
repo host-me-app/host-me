f038870c930ffce05668d4029cfb0adf774a9ab5
jules20022000 <jules.maglione@epfl.ch>
2022-05-10 10:42:13 +0200

    call test

--- app/src/androidTest/java/ch/epfl/sweng/hostme/chat/CallTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/hostme/chat/CallTest.java
@@ -1,6 +1,5 @@
 package ch.epfl.sweng.hostme.chat;
 
-import static androidx.test.core.app.ApplicationProvider.getApplicationContext;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
@@ -8,12 +7,9 @@ import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 import android.Manifest;
-import android.content.Intent;
 
-import androidx.test.core.app.ActivityScenario;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.espresso.contrib.RecyclerViewActions;
-import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.GrantPermissionRule;
@@ -53,38 +49,32 @@ public class CallTest {
     public ActivityScenarioRule<LogInActivity> rule = new ActivityScenarioRule<>(LogInActivity.class);
 
     @Test
-    public void callUser() {
-        Intent intent = new Intent(getApplicationContext(), LogInActivity.class);
-        Intents.init();
-        try (ActivityScenario<LogInActivity> scenario = ActivityScenario.launch(intent)) {
-            String mail = "testlogin@gmail.com";
-            String password = "fakePassword1!";
+    public void callUser() throws InterruptedException {
 
-            onView(withId(R.id.userName)).perform(typeText(mail), closeSoftKeyboard());
-            onView(withId(R.id.pwd)).perform(typeText(password), closeSoftKeyboard());
-            onView(withId(R.id.logInButton)).perform(click());
-            Thread.sleep(1000);
+        String mail = "testlogin@gmail.com";
+        String password = "fakePassword1!";
 
-            onView(withId(R.id.recyclerView)).perform(
+        onView(withId(R.id.userName)).perform(typeText(mail), closeSoftKeyboard());
+        onView(withId(R.id.pwd)).perform(typeText(password), closeSoftKeyboard());
+        onView(withId(R.id.logInButton)).perform(click());
+        Thread.sleep(1000);
+
+        onView(withId(R.id.recyclerView)).perform(
                     RecyclerViewActions.actionOnItemAtPosition(0, click()));
-            Thread.sleep(500);
-            onView(withId(R.id.contact_user_button)).perform(click());
+        Thread.sleep(500);
+        onView(withId(R.id.contact_user_button)).perform(click());
 
-            onView(withId(R.id.launchButt)).perform(click());
-            Thread.sleep(1000);
-            onView(withId(R.id.audioBtn)).perform(click());
-            onView(withId(R.id.audioBtn)).perform(click());
-            onView(withId(R.id.audioBtn)).perform(click());
-            onView(withId(R.id.videoBtn)).perform(click());
-            onView(withId(R.id.videoBtn)).perform(click());
-            onView(withId(R.id.switch_camera)).perform(click());
-            onView(withId(R.id.switch_camera)).perform(click());
-            Thread.sleep(500);
-            onView(withId(R.id.leaveBtn)).perform(click());
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        Intents.release();
+        onView(withId(R.id.launchButt)).perform(click());
+        Thread.sleep(1000);
+        onView(withId(R.id.audioBtn)).perform(click());
+        onView(withId(R.id.audioBtn)).perform(click());
+        onView(withId(R.id.audioBtn)).perform(click());
+        onView(withId(R.id.videoBtn)).perform(click());
+        onView(withId(R.id.videoBtn)).perform(click());
+        onView(withId(R.id.switch_camera)).perform(click());
+        onView(withId(R.id.switch_camera)).perform(click());
+        Thread.sleep(500);
+        onView(withId(R.id.leaveBtn)).perform(click());
     }
 
 }

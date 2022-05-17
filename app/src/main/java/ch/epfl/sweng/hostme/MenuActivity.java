b3eaad5119892851320ea177f8d99cf31f5a1b7b
Paul Nadal <paul.nadal@epfl.ch>
2022-05-18 01:12:11 +0200

    Coverage improvement

--- app/src/main/java/ch/epfl/sweng/hostme/MenuActivity.java
+++ app/src/main/java/ch/epfl/sweng/hostme/MenuActivity.java
@@ -139,8 +139,6 @@ public class MenuActivity extends AppCompatActivity {
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
         super.onActivityResult(requestCode, resultCode, intent);
-
-
         if (requestCode == REQ_IMAGE) {
             ListImage.onAcceptImage(resultCode, intent.getData());
         }

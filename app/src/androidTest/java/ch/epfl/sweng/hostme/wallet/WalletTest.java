1c939066b407c6869577f2b6437830d6d80a8770
Paul Nadal <paul.nadal@epfl.ch>
2022-05-18 17:42:57 +0200

    coverage improvement on wallet

--- app/src/androidTest/java/ch/epfl/sweng/hostme/wallet/WalletTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/hostme/wallet/WalletTest.java
@@ -4,9 +4,22 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.Intents.intending;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasAction;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
+import static org.junit.Assert.assertNotNull;
+
+import android.app.Activity;
+import android.app.Instrumentation;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.pdf.PdfDocument;
+import android.net.Uri;
 
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.core.app.ApplicationProvider;
@@ -20,6 +33,10 @@ import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 import ch.epfl.sweng.hostme.LogInActivity;
 import ch.epfl.sweng.hostme.R;
@@ -76,6 +93,9 @@ public class WalletTest {
         Intent intent = new Intent(ApplicationProvider.getApplicationContext(), LogInActivity.class);
         Intents.init();
         try (ActivityScenario<LogInActivity> scenario = ActivityScenario.launch(intent)) {
+            savePickedPDF();
+            intending(hasAction(Intent.ACTION_CHOOSER)).respondWith(getPDFResult());
+
             String mail = "testlogin@gmail.com";
             String password = "fakePassword1!";
 
@@ -90,9 +110,55 @@ public class WalletTest {
             onView(withId(R.id.wallet_button)).perform(click());
             onView(withId(R.id.button_browse_salary_slips)).perform(click());
             Thread.sleep(1000);
+            onView(withId(R.id.button_download_salary_slips)).perform(click());
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         Intents.release();
+    }
+
+
+    private Instrumentation.ActivityResult getPDFResult() {
+        Intent resultData = new Intent();
+        File dir = ApplicationProvider.getApplicationContext().getExternalCacheDir();
+        File file = new File(dir.getPath(), "file.pdf");
+        Uri uri = Uri.fromFile(file);
+        resultData.setData(uri);
+        return new Instrumentation.ActivityResult(Activity.RESULT_OK, resultData);
+    }
+
+    public static Bitmap drawableToBitmap (Drawable drawable) {
+
+        if (drawable instanceof BitmapDrawable) {
+            return ((BitmapDrawable)drawable).getBitmap();
+        }
+
+        Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+
+        return bitmap;
+    }
+
+    private void savePickedPDF() {
+        Drawable d = ApplicationProvider.getApplicationContext().getResources().getDrawable(R.drawable.add_icon);
+        Bitmap bm = drawableToBitmap(d);
+        PdfDocument pdfDocument = new PdfDocument();
+        PdfDocument.PageInfo myPageInfo = new PdfDocument.PageInfo.Builder(960,1280,1).create();
+        PdfDocument.Page page = pdfDocument.startPage(myPageInfo);
+        page.getCanvas().drawBitmap(bm,0,0, null);
+        pdfDocument.finishPage(page);
+        File dir = ApplicationProvider.getApplicationContext().getExternalCacheDir();
+        File file = new File(dir.getPath(), "file.pdf");
+        FileOutputStream outStream = null;
+        try{
+            outStream  = new FileOutputStream(file);
+            pdfDocument.writeTo(outStream);
+            outStream.flush();
+            outStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }

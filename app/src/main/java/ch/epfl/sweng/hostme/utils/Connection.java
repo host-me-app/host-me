a2cd1cb48e889bcddc3b3c9ba4954de1848027af
Scott Williams <92975604+Scott2Williams@users.noreply.github.com>
2022-05-20 09:48:47 +0200

    Update recycler on apartment generation and begin offline behavior for add fragment (#188)

---
+++ app/src/main/java/ch/epfl/sweng/hostme/utils/Connection.java
@@ -0,0 +1,22 @@
+package ch.epfl.sweng.hostme.utils;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class Connection {
+    /**
+     * Check if the user has Internet connection
+     *
+     * @return true if user has connection, false otherwise
+     */
+    @SuppressLint("MissingPermission")
+    public static boolean online(Activity activity) {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) activity.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+}

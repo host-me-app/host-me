0de7e39be1c9951e4c8ac05b06c7560c54d788b7
jules20022000 <jules.maglione@epfl.ch>
2022-05-26 20:24:05 +0200

    final version scrolling images

--- app/src/androidTest/java/ch/epfl/sweng/hostme/chat/UsersActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/hostme/chat/UsersActivityTest.java
@@ -1,9 +1,6 @@
 package ch.epfl.sweng.hostme.chat;
 
 import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
@@ -13,7 +10,6 @@ import android.content.Intent;
 
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.core.app.ApplicationProvider;
-import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
@@ -24,7 +20,6 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import ch.epfl.sweng.hostme.LogInActivity;
 import ch.epfl.sweng.hostme.R;
 import ch.epfl.sweng.hostme.database.Auth;
 import ch.epfl.sweng.hostme.database.Database;
@@ -64,34 +59,4 @@ public class UsersActivityTest {
         Intents.release();
     }
 
-    @Test
-    public void UsersDisplayed() {
-        Intent intent = new Intent(ApplicationProvider.getApplicationContext(), LogInActivity.class);
-        Intents.init();
-        try (ActivityScenario<UsersActivity> scenario = ActivityScenario.launch(intent)) {
-            String mail = "testlogin@gmail.com";
-            String password = "fakePassword1!";
-
-            onView(withId(R.id.userName)).perform(typeText(mail), closeSoftKeyboard());
-            onView(withId(R.id.pwd)).perform(typeText(password), closeSoftKeyboard());
-            onView(withId(R.id.logInButton)).perform(click());
-            Thread.sleep(1000);
-
-            onView(withId(R.id.navigation_messages)).perform(click());
-            Thread.sleep(1000);
-
-            onView(withId(R.id.contactButton)).check(matches(isDisplayed()));
-            onView(withId(R.id.contactButton)).perform(click());
-            Thread.sleep(1000);
-
-            onView(withId(R.id.usersRecyclerView)).check(matches(isDisplayed()));
-            onView(withId(R.id.usersRecyclerView))
-                    .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
-            Thread.sleep(1000);
-
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        Intents.release();
-    }
 }

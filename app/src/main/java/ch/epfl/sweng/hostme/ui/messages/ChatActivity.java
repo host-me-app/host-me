388d28f7453ac5f8615cb85b3721d6877e230718
Paul Nadal <paul.nadal@epfl.ch>
2022-05-30 22:11:36 +0200

    change ui messages

--- app/src/main/java/ch/epfl/sweng/hostme/ui/messages/ChatActivity.java
+++ app/src/main/java/ch/epfl/sweng/hostme/ui/messages/ChatActivity.java
@@ -29,7 +29,6 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
